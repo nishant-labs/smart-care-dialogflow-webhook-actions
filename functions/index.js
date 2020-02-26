a5767d759a0bc253fb9b17c6938b49f15ac4b695
Nishant Kumar Sinha <nishantsinha-5@users.noreply.github.com>
2020-02-26 08:59:53 +0000

    task: define intent fulfilment

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,37 @@
-const { dialogflow } = require('actions-on-google');
+const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
 
-const app = dialogflow({ debug: true });
+const app = dialogflow({debug: true});
 
-app.intent('Default Welcome Intent', conversation => {
-	conversation.close('Hello, World!');
-	// Complete your fulfillment logic and
-	// send a response when the function is done executing
+app.intent('AskForHelp_General', (conversation) => {
+  const {contexts} = conversation;
+  const questionContext = contexts.get('context-welcome-question');
+  if (questionContext && questionContext.parameters.fullName) {
+    conversation.ask(
+        `What happened ${questionContext.parameters.fullName} ? could you please elaborate more!!`
+    );
+  } else {
+    conversation.ask(
+        'Of course, but can you answer some questions before I can help you. What is your name?'
+    );
+  }
+});
+
+app.intent('WelcomeQuestions', (conversation) => {
+  const {contexts} = conversation;
+  const askContext = contexts.get('context-ask-for-help');
+  if (askContext && askContext.parameters.helpAsked) {
+    conversation.ask(
+        `Thanks $fullName , Let me see what I can do`
+    );
+  } else {
+    conversation.ask(`Thanks $fullName , How can I help you?`);
+  }
+});
+
+app.catch((conversation, error) => {
+  console.error(error);
+  conversation.ask('I encountered a glitch. Can you say that again?');
 });
 
 exports.yourAction = functions.https.onRequest(app);
